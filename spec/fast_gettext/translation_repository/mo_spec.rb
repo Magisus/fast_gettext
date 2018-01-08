513bd192abaae71b5fa26ddd76b24a62b63ef85f
Maggie Dreyer <maggie@puppet.com>
2018-01-08 10:57:05 -0800

    Preserve Ruby 1.8 compatibility
    This commit updates the file encoding detection to only be performed on
    Ruby 1.9 and above, and updates the specs accordingly, so they expect
    the correct arguments. This is preserver compatibility with Ruby
    Enterprise Edition and Ruby 1.8.7.

--- spec/fast_gettext/translation_repository/mo_spec.rb
+++ spec/fast_gettext/translation_repository/mo_spec.rb
@@ -53,7 +53,7 @@ describe 'FastGettext::TranslationRepository::Mo' do
 
   it "can work in SAFE mode" do
     pending_if RUBY_VERSION > "2.0" do
-      `ruby spec/cases/safe_mode_can_handle_locales.rb 2>&1`.should == 'true'
+      `ruby spec/cases/safe_mode_can_handle_locales.rb 2>&1`.should =~ /true/
     end
   end
 end
